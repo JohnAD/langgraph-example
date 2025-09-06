f54d33d65f5707bfa82d56ee41c3ceed821df7ae
John Dupuy <john.dupuy@veteransunited.com>
2025-09-06 16:55:15 -0500

    more play

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -3,9 +3,10 @@ from __future__ import annotations
 import os
 from typing import Literal, Annotated, TypedDict
 
-from braintrust import init_logger
+from braintrust import init_logger, wrap_openai, traced
 from braintrust_langchain import BraintrustCallbackHandler, set_global_handler
 from langchain_core.messages import AnyMessage
+from langchain_core.runnables import RunnableConfig
 from langchain_core.tools import tool
 from langchain_openai import ChatOpenAI
 from langgraph.constants import END
@@ -18,15 +19,15 @@ from src.agent.weathergov import get_weather_forecast
 from src.agent.prompts import get_leading_prompts
 from src.agent.weathergov import summarize_forecasts
 
-init_logger(project="My Project", api_key=os.environ.get("BRAINTRUST_API_KEY"))
-handler = BraintrustCallbackHandler()
-set_global_handler(handler)
+bt_logger = init_logger(project="My Project", api_key="sk-dLnw91yQDDTutA9IhW0ts0TaklYZVDiTxZ9ku9CBLD18ruaY")
+bt_handler = BraintrustCallbackHandler(bt_logger)
+set_global_handler(bt_handler)
 
 class State(TypedDict):
     messages: Annotated[list[AnyMessage], add_messages]
 
-
 @tool
+# @traced
 def get_weather(gridId: str, x: str, y: str) -> str:
     """Get the weather for a given location expressed as gridId, x, y."""
     forecasts = get_weather_forecast(gridId, x, y)
@@ -39,18 +40,24 @@ tool_node = ToolNode(tools)
 
 llm = ChatOpenAI(
     api_key=SecretStr(os.environ['TA_OPENAI_KEY']),
-    model="gpt-4o"
+    model="gpt-4o",
+    callbacks=[bt_handler]
 )
 
 llm_with_tools = llm.bind_tools(tools)
 
 ASSISTANT = "assistant"
 
+runnable_config_for_openai = RunnableConfig()
+runnable_config_for_openai["callbacks"] = [bt_handler]
 
-async def assistant(state: State):
+def assistant(state: State):
     all_messages = get_leading_prompts() + list(state["messages"])
     return {
-        "messages": [await llm_with_tools.ainvoke(all_messages)]
+        "messages": [llm_with_tools.ainvoke(
+            all_messages,
+            config=runnable_config_for_openai
+        )]
     }
 
 
