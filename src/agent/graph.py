5b1190d4f7bb9affaffec22cbe9c83882a5a567b
John Dupuy <john.dupuy@veteransunited.com>
2025-09-07 14:29:54 -0500

    switch to otel

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -2,36 +2,44 @@ from __future__ import annotations
 
 import os
 from typing import Literal, Annotated, TypedDict
-
-from braintrust import init_logger, wrap_openai, traced
-from braintrust_langchain import BraintrustCallbackHandler, set_global_handler
+from braintrust.otel import BraintrustSpanProcessor
 from langchain_core.messages import AnyMessage
-from langchain_core.runnables import RunnableConfig
 from langchain_core.tools import tool
 from langchain_openai import ChatOpenAI
 from langgraph.constants import END
 from langgraph.graph import StateGraph
 from langgraph.graph.message import add_messages
 from langgraph.prebuilt import ToolNode
+from opentelemetry import trace
+from opentelemetry.sdk.trace import TracerProvider
 from pydantic import SecretStr
 
 from src.agent.weathergov import get_weather_forecast
 from src.agent.prompts import get_leading_prompts
 from src.agent.weathergov import summarize_forecasts
 
-bt_logger = init_logger(project="My Project", api_key="sk-dLnw91yQDDTutA9IhW0ts0TaklYZVDiTxZ9ku9CBLD18ruaY")
-bt_handler = BraintrustCallbackHandler(bt_logger)
-set_global_handler(bt_handler)
+provider = TracerProvider()
+provider.add_span_processor(BraintrustSpanProcessor("My Project"))
+trace.set_tracer_provider(provider)
+
+tracer = trace.get_tracer(__name__)
 
 class State(TypedDict):
     messages: Annotated[list[AnyMessage], add_messages]
 
+
 @tool
-# @traced
+@tracer.start_as_current_span("get_weather")
 def get_weather(gridId: str, x: str, y: str) -> str:
     """Get the weather for a given location expressed as gridId, x, y."""
     forecasts = get_weather_forecast(gridId, x, y)
-    return summarize_forecasts(forecasts)
+    result = summarize_forecasts(forecasts)
+    trace.get_current_span().set_attributes(
+        {
+            "foo": "bar",
+        }
+    )
+    return result
 
 
 TOOLS = "tools"
@@ -40,23 +48,20 @@ tool_node = ToolNode(tools)
 
 llm = ChatOpenAI(
     api_key=SecretStr(os.environ['TA_OPENAI_KEY']),
-    model="gpt-4o",
-    callbacks=[bt_handler]
+    model="gpt-4o"
 )
 
 llm_with_tools = llm.bind_tools(tools)
 
 ASSISTANT = "assistant"
 
-runnable_config_for_openai = RunnableConfig()
-runnable_config_for_openai["callbacks"] = [bt_handler]
 
+@tracer.start_as_current_span("assistant")
 def assistant(state: State):
     all_messages = get_leading_prompts() + list(state["messages"])
     return {
         "messages": [llm_with_tools.ainvoke(
-            all_messages,
-            config=runnable_config_for_openai
+            all_messages
         )]
     }
 
