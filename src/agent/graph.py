dd41adacfaf330e7829bbb052906113270bdd9c7
John Dupuy <john.dupuy@veteransunited.com>
2025-09-06 21:18:35 -0500

    trying wrapper

--- src/agent/graph.py
+++ src/agent/graph.py
@@ -2,43 +2,44 @@ import asyncio
 import os
 from typing import Dict
 
-from braintrust import init_logger
+from braintrust import init_logger, traced, wrap_openai
 from braintrust_langchain import BraintrustCallbackHandler, set_global_handler
+from langchain_core.runnables import RunnableConfig
 from langchain_openai import ChatOpenAI
 from langgraph.graph import END, START, StateGraph
 from pydantic import SecretStr
 
 
-async def main():
-    init_logger(project="My Project", api_key="sk-dLnw91yQDDTutA9IhW0ts0TaklYZVDiTxZ9ku9CBLD18ruaY")
+init_logger(project="My Project", api_key="sk-dLnw91yQDDTutA9IhW0ts0TaklYZVDiTxZ9ku9CBLD18ruaY")
 
-    handler = BraintrustCallbackHandler()
-    set_global_handler(handler)
+# handler = BraintrustCallbackHandler()
+# set_global_handler(handler)
 
-    # Use any LangChain supported model here
-    model = ChatOpenAI(api_key=SecretStr(os.environ['TA_OPENAI_KEY']), model="gpt-4o-mini")
+# Use any LangChain supported model here
+model = wrap_openai(ChatOpenAI(
+    api_key=SecretStr(os.environ['TA_OPENAI_KEY']),
+    model="gpt-4o-mini"
+))
 
-    def say_hello(state: Dict[str, str]):
-        response = model.invoke("Say hello")
-        return response.content
+@traced
+def say_hello(state: Dict[str, str]):
+    response = model.invoke("Say hello")
+    return response.content
 
-    def say_bye(state: Dict[str, str]):
-        print("From the 'sayBye' node: Bye world!")
-        return "Bye"
+@traced
+def say_bye(state: Dict[str, str]):
+    print("From the 'sayBye' node: Bye world!")
+    return "Bye"
 
-    # Create the state graph
-    workflow = (
-        StateGraph(state_schema=Dict[str, str])
-        .add_node("sayHello", say_hello)
-        .add_node("sayBye", say_bye)
-        .add_edge(START, "sayHello")
-        .add_edge("sayHello", "sayBye")
-        .add_edge("sayBye", END)
-    )
+# Create the state graph
+workflow = (
+    StateGraph(state_schema=Dict[str, str])
+    .add_node("sayHello", say_hello)
+    .add_node("sayBye", say_bye)
+    .add_edge(START, "sayHello")
+    .add_edge("sayHello", "sayBye")
+    .add_edge("sayBye", END)
+)
 
-    graph = workflow.compile()
-    await graph.ainvoke({})
-    return graph
-
-
-graph = asyncio.run(main())
+graph = workflow.compile()
+# asyncio.run(graph.ainvoke({"foo": "bar"}))
